4cb43760aeb3af0aff2702a6dc72de0b47d04d2c
TAYYAB ALI <tayyabali78013@gmail.com>
2023-05-14 23:30:19 +0500

    first commit

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+require("./db/conn");
+
+const router = require("./routes/router");
+
+const cors = require("cors");
+
+const cookiParser = require("cookie-parser");
+
+const port = 8010;
+
+// app.get("/",(req,res)=>{
+//     res.status(201).json("server created")
+// });
+
+app.use(express.json());
+
+app.use(cookiParser());
+
+app.use(cors());
+
+app.use(router);
+
+app.listen(port, () => {
+
+    console.log(`Backend Server Started On Port http://localhost:${port}`);
+
+});
